8954a213a91f8a36c9acdcf6866382bbe7cf1d83
Adam Dai <adamdai97@gmail.com>
2025-07-09 09:51:04 -0700

    adding shadow-splat-2dgs back to config

--- shadow_splat/config.py
+++ shadow_splat/config.py
@@ -11,7 +11,7 @@ from shadow_splat.model import ShadowSplatModelConfig
 from shadow_splat.dataparser import ShadowSplatDataParserConfig
 from shadow_splat.datamanager import ShadowSplatDataManagerConfig
 from shadow_splat.pipeline import ShadowSplatPipelineConfig
-# from shadow_splat.model_2dgs import ShadowSplat2DGSModelConfig
+from shadow_splat.model_2dgs import ShadowSplat2DGSModelConfig
 
 # NOTE: The ShadowSplatTrainer import is needed for some reason to make the viewer work
 from shadow_splat.trainer import ShadowSplatTrainerConfig, ShadowSplatTrainer
@@ -89,68 +89,68 @@ shadow_splat = MethodSpecification(
     description="Config for ShadowSplat",
 )
 
-# shadow_splat_2dgs = MethodSpecification(
-#     ShadowSplatTrainerConfig(
-#         method_name="shadow-splat-2dgs",
-#         steps_per_eval_image=100,
-#         steps_per_eval_batch=0,
-#         steps_per_save=700,
-#         steps_per_eval_all_images=1000,
-#         max_num_iterations=30000,
-#         mixed_precision=False,
-#         pipeline=ShadowSplatPipelineConfig(
-#             datamanager=ShadowSplatDataManagerConfig(
-#                 dataparser=ShadowSplatDataParserConfig(load_3D_points=True),
-#                 cache_images_type="uint8",
-#             ),
-#             model=ShadowSplat2DGSModelConfig(),
-#         ),
-#         optimizers={
-#             "means": {
-#                 "optimizer": AdamOptimizerConfig(lr=1.6e-4, eps=1e-15),
-#                 "scheduler": ExponentialDecaySchedulerConfig(
-#                     lr_final=1.6e-6,
-#                     max_steps=30000,
-#                 ),
-#             },
-#             "features_dc": {
-#                 "optimizer": AdamOptimizerConfig(lr=0.0025, eps=1e-15),
-#                 "scheduler": None,
-#             },
-#             "features_rest": {
-#                 "optimizer": AdamOptimizerConfig(lr=0.0025 / 20, eps=1e-15),
-#                 "scheduler": None,
-#             },
-#             "opacities": {
-#                 "optimizer": AdamOptimizerConfig(lr=0.05, eps=1e-15),
-#                 "scheduler": None,
-#             },
-#             "scales": {
-#                 "optimizer": AdamOptimizerConfig(lr=0.005, eps=1e-15),
-#                 "scheduler": None,
-#             },
-#             "quats": {"optimizer": AdamOptimizerConfig(lr=0.001, eps=1e-15), "scheduler": None},
-#             "camera_opt": {
-#                 "optimizer": AdamOptimizerConfig(lr=1e-4, eps=1e-15),
-#                 "scheduler": ExponentialDecaySchedulerConfig(
-#                     lr_final=5e-7, max_steps=30000, warmup_steps=1000, lr_pre_warmup=0
-#                 ),
-#             },
-#             "bilateral_grid": {
-#                 "optimizer": AdamOptimizerConfig(lr=2e-3, eps=1e-15),
-#                 "scheduler": ExponentialDecaySchedulerConfig(
-#                     lr_final=1e-4, max_steps=30000, warmup_steps=1000, lr_pre_warmup=0
-#                 ),
-#             },
-#             "intensity": {"optimizer": AdamOptimizerConfig(lr=0.001, eps=1e-15), "scheduler": None},
-#             "cutoff": {"optimizer": AdamOptimizerConfig(lr=0.001, eps=1e-15), "scheduler": None},
-#             "variance_factor": {
-#                 "optimizer": AdamOptimizerConfig(lr=0.001, eps=1e-15),
-#                 "scheduler": None,
-#             },
-#         },
-#         viewer=ViewerConfig(num_rays_per_chunk=1 << 15),
-#         vis="viewer",
-#     ),
-#     description="Config for ShadowSplat2DGS",
-# )
+shadow_splat_2dgs = MethodSpecification(
+    ShadowSplatTrainerConfig(
+        method_name="shadow-splat-2dgs",
+        steps_per_eval_image=100,
+        steps_per_eval_batch=0,
+        steps_per_save=700,
+        steps_per_eval_all_images=1000,
+        max_num_iterations=30000,
+        mixed_precision=False,
+        pipeline=ShadowSplatPipelineConfig(
+            datamanager=ShadowSplatDataManagerConfig(
+                dataparser=ShadowSplatDataParserConfig(load_3D_points=True),
+                cache_images_type="uint8",
+            ),
+            model=ShadowSplat2DGSModelConfig(),
+        ),
+        optimizers={
+            "means": {
+                "optimizer": AdamOptimizerConfig(lr=1.6e-4, eps=1e-15),
+                "scheduler": ExponentialDecaySchedulerConfig(
+                    lr_final=1.6e-6,
+                    max_steps=30000,
+                ),
+            },
+            "features_dc": {
+                "optimizer": AdamOptimizerConfig(lr=0.0025, eps=1e-15),
+                "scheduler": None,
+            },
+            "features_rest": {
+                "optimizer": AdamOptimizerConfig(lr=0.0025 / 20, eps=1e-15),
+                "scheduler": None,
+            },
+            "opacities": {
+                "optimizer": AdamOptimizerConfig(lr=0.05, eps=1e-15),
+                "scheduler": None,
+            },
+            "scales": {
+                "optimizer": AdamOptimizerConfig(lr=0.005, eps=1e-15),
+                "scheduler": None,
+            },
+            "quats": {"optimizer": AdamOptimizerConfig(lr=0.001, eps=1e-15), "scheduler": None},
+            "camera_opt": {
+                "optimizer": AdamOptimizerConfig(lr=1e-4, eps=1e-15),
+                "scheduler": ExponentialDecaySchedulerConfig(
+                    lr_final=5e-7, max_steps=30000, warmup_steps=1000, lr_pre_warmup=0
+                ),
+            },
+            "bilateral_grid": {
+                "optimizer": AdamOptimizerConfig(lr=2e-3, eps=1e-15),
+                "scheduler": ExponentialDecaySchedulerConfig(
+                    lr_final=1e-4, max_steps=30000, warmup_steps=1000, lr_pre_warmup=0
+                ),
+            },
+            "intensity": {"optimizer": AdamOptimizerConfig(lr=0.001, eps=1e-15), "scheduler": None},
+            "cutoff": {"optimizer": AdamOptimizerConfig(lr=0.001, eps=1e-15), "scheduler": None},
+            "variance_factor": {
+                "optimizer": AdamOptimizerConfig(lr=0.001, eps=1e-15),
+                "scheduler": None,
+            },
+        },
+        viewer=ViewerConfig(num_rays_per_chunk=1 << 15),
+        vis="viewer",
+    ),
+    description="Config for ShadowSplat2DGS",
+)
